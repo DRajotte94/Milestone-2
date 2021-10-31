29e193161b0fda8c2ffa163a3ba0a2a59977295b
drajo <drajo@DESKTOP-LSTFBT7>
2021-10-31 10:48:57 -0700

    Milestone 3, CST150

--- Milestone 2/Program.cs
+++ Milestone 2/Program.cs
@@ -10,27 +10,263 @@ namespace Milestone_2
     {
         static void Main(string[] args)
         {
-            // create two objects for testing purposes
+            // create several objects for testing purposes
             Bike bike1 = new Bike("Diamondback", "small", "full", "red", 2);
             Bike bike2 = new Bike("E-Caliber", "medium", "rear", "black", 1);
+            Bike bike3 = new Bike("Eclipse", "small", "rear", "white", 1);
+            Bike bike4 = new Bike("Diamondback", "medium", "full", "black", 2);
 
-            // Print bike 1 to console, edit it, then print again
-            Console.WriteLine("Bike 1: " + bike1.ToString());
-            bike1.setName("Diamonback Bolt");
-            bike1.setColor("blue/white");
-            bike1.setQuantity(1);
-            Console.WriteLine("Bike 1: " + bike1.ToString());
+            // inventory list of bikes (several bikes pre loaded into list for testing)
+            List<Bike> inventory = new List<Bike>();
+            inventory.Add(bike1);
+            inventory.Add(bike2);
+            inventory.Add(bike3);
+            inventory.Add(bike4);
 
-            Console.WriteLine(" "); // blank line to seperate objects
+            Boolean loop = true;
+            while (loop == true)
+            {
+                int input;
+                Console.WriteLine("\n-------------------------------------------\n");
+                Console.WriteLine("Enter one of the following options: ");
+                Console.WriteLine("1: View Inventory");
+                Console.WriteLine("2: Add Item");
+                Console.WriteLine("3: Remove Item");
+                Console.WriteLine("4: Adjust Item Quantity");
+                Console.WriteLine("5: Search Inventory");
+                Console.WriteLine("0: Exit");
+                Console.Write("Enter: ");
 
-            // Print bike 1 to console, edit it, then print again
-            Console.WriteLine("Bike 2: " + bike2.ToString());
-            bike2.setSize("small");
-            bike2.setSuspension("full");
-            Console.WriteLine("Bike 2: " + bike2.ToString());
+                if (int.TryParse(Console.ReadLine(), out input) && input >= 0 && input <= 5) // returns error if input is not a valid int
+                {
+                    // provide controls based on input
+                    if (input == 1) // view inventory
+                    {
+                        if (inventory.Count <= 0)
+                        {
+                            Console.WriteLine("\nInventory is empty");
+                        }
+                        else
+                        {
+                            int count = 1;
+                            foreach (Bike i in inventory)
+                            {
+                                Console.Write($"\n{count}: {i.ToString()}");
+                                count++;
+                            }
+                        }
+                    }
+                    else if (input == 2) // add item
+                    {
+                        // get variables to build object
+                        Console.Write("\nEnter item name: ");
+                        String name = Console.ReadLine();
+                        Console.Write("Enter item size: ");
+                        String size = Console.ReadLine();
+                        Console.Write("Enter item suspension: ");
+                        String suspension = Console.ReadLine();
+                        Console.Write("Enter item color: ");
+                        String color = Console.ReadLine();
+                        Console.Write("Enter item quantity: ");
+                        int quantity = Convert.ToInt32(Console.ReadLine());
 
-            // prevents console from closing until enter key is pressed
-            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                        // build object
+                        Bike bike = new Bike(name, size, suspension, color, quantity);
+
+                        // add to inventory list
+                        InventoryManager.addItem(inventory, bike);
+                    }
+                    else if (input == 3) // remove item
+                    {
+                        Console.Write("\nEnter the inventory # of the item you want to remove: ");
+                        int item;
+                        if (int.TryParse(Console.ReadLine(), out item) && item > 0 && item <= inventory.Count) 
+                        {
+                            // remove item from list
+                            InventoryManager.removeItem(inventory, item);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                        }
+                        
+                    }
+                    else if (input == 4) // adjust quantity
+                    {
+                        Console.Write("\nEnter the inventory # of the item: ");
+                        int item;
+                        if (int.TryParse(Console.ReadLine(), out item) && item > 0 && item <= inventory.Count)
+                        {
+                            // edit the quantity of the item
+                            InventoryManager.editQuantity(inventory, item);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                        }
+                    }
+                    else if (input == 5) // search inventory
+                    {
+                        Console.WriteLine("\nSelect search criteria: ");
+                        Console.WriteLine("1: Name");
+                        Console.WriteLine("2: Size");
+                        Console.WriteLine("3: Suspension");
+                        Console.WriteLine("4: Color");
+                        Console.Write("Enter: ");
+
+                        int entry = 0;
+                        if (int.TryParse(Console.ReadLine(), out entry) && entry > 0 && entry <= 5)
+                        {
+                            if (entry == 1) // name
+                            {
+                                // get name being searched
+                                Console.Write("\nEnter name: ");
+                                String name = Console.ReadLine();
+
+                                // call name search method
+                                InventoryManager.searchName(inventory, name);
+                            }
+                            if (entry == 2) // size
+                            {
+                                // get size being searched
+                                Console.Write("\nEnter size (small, medium, large): ");
+                                String size = Console.ReadLine();
+
+                                // call name search method
+                                InventoryManager.searchSize(inventory, size);
+                            }
+                            if (entry == 3) // suspension
+                            {
+                                // get size being searched
+                                Console.Write("\nEnter suspension (rear, full): ");
+                                String suspension = Console.ReadLine();
+
+                                // call name search method
+                                InventoryManager.searchSuspension(inventory, suspension);
+                            }
+                            if (entry == 4) // color
+                            {
+                                // get size being searched
+                                Console.Write("\nEnter color: ");
+                                String color = Console.ReadLine();
+
+                                // call name search method
+                                InventoryManager.searchColor(inventory, color);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input"); // invalid input error handling
+                        }
+                    }
+                    else if (input == 0) // end loop
+                    {
+                        loop = false;
+                    }
+                }
+                else // invalid input error handling
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+
+    class InventoryManager
+    {
+        // method for adding item to list
+        public static void addItem(List<Bike> list, Bike bike)
+        {
+            // if item is not present on list, add
+            if (!list.Contains(bike))
+            {
+                list.Add(bike);
+            }
+        }
+
+        // method for removing item from list
+        public static void removeItem(List<Bike> list, int item)
+        {
+            Bike bike = list[item - 1];
+            list.Remove(bike);
+        }
+
+        // method for restocking items
+        public static void editQuantity(List<Bike> list, int item)
+        {
+            Bike bike = list[item];
+            Console.Write("\nEnter the new quantity: ");
+
+            int quantity;
+            if (int.TryParse(Console.ReadLine(), out quantity) && quantity >= 0)
+            {
+                bike.setQuantity(quantity);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+        // method for searching by name
+        public static void searchName(List<Bike> list, String name)
+        {
+            int i = 0;
+            foreach (Bike bike in list)
+            {
+                // compare list items name to the searched name
+                if (string.Equals(list[i].getName(), name, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{i + 1}: {bike.ToString()}");
+                }
+
+                i++;
+            }
+        }
+
+        // method for searching by size
+        public static void searchSize(List<Bike> list, String size)
+        {
+            int i = 0;
+            foreach (Bike bike in list)
+            {
+                if (string.Equals(list[i].getSize(), size, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{i + 1}: {bike.ToString()}");
+                }
+
+                i++;
+            }
+        }
+
+        // method for searching by suspension
+        public static void searchSuspension(List<Bike> list, String suspension)
+        {
+            int i = 0;
+            foreach (Bike bike in list)
+            {
+                if (string.Equals(list[i].getSuspension(), suspension, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{i + 1}: {bike.ToString()}");
+                }
+
+                i++;
+            }
+        }
+
+        // method for searching by color
+        public static void searchColor(List<Bike> list, String color)
+        {
+            int i = 0;
+            foreach (Bike bike in list)
+            {
+                if (string.Equals(list[i].getColor(), color, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{i + 1}: {bike.ToString()}");
+                }
+
+                i++;
+            }
         }
     }
 
